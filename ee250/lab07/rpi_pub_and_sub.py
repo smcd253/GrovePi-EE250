fe9765bb90897b8f9c6011eaaf26cc1a1d425a87
ee250 <shmcdono@usc.edu>
2018-03-20 18:40:50 -0700

    part 1

--- ee250/lab07/rpi_pub_and_sub.py
+++ ee250/lab07/rpi_pub_and_sub.py
@@ -5,14 +5,30 @@ Run rpi_pub_and_sub.py on your Raspberry Pi."""
 import paho.mqtt.client as mqtt
 import time
 
+import grovepi
+from grovepi import *
+
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
 
     #subscribe to topics of interest here
+    client.subscribe("anrg-pi9/defaultCallback")
+    client.subscribe("anrg-pi9/ultrasonic")
+    client.message_callback_add("anrg-pi9/ultrasonic", ultrasonic)
 
 #Default message callback. Please use custom callbacks.
 def on_message(client, userdata, msg):
     print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
+
+def ultrasonic(client, userdata, message):
+    #the third argument is 'message' here unlike 'msg' in on_message 
+    print("ultrasonic: " + message.topic + " " + "\"" + 
+        str(message.payload, "utf-8") + "\"")
+    print("ultrasonic: message.payload is of type " + 
+          str(type(message.payload)))
+
+# define ultrasonic ranger port
+ultrasonic_ranger = 4
 
 if __name__ == '__main__':
     #this section is covered in publisher_and_subscriber_example.py
@@ -23,7 +39,21 @@ if __name__ == '__main__':
     client.loop_start()
 
     while True:
-        print("delete this line")
+        ################ PUBLISH ###############
+        try:
+            # Read distance value from Ultrasonic
+            dist = grovepi.ultrasonicRead(ultrasonic_ranger)
+            data = str(dist)
+
+        except TypeError:
+            data = "TypeError"
+        except IOError:
+            data = "IOError"
+
+        
+
+
+
         time.sleep(1)
             
 
--- ee250/lab07/vm_subscriber.py
+++ ee250/lab07/vm_subscriber.py
@@ -5,14 +5,26 @@ Run vm_subscriber.py in a separate terminal on your VM."""
 import paho.mqtt.client as mqtt
 import time
 
+
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
 
     #subscribe to the ultrasonic ranger topic here
+    client.subscribe("anrg-pi9/defaultCallback")
+    client.subscribe("anrg-pi9/ultrasonic")
+    client.message_callback_add("anrg-pi9/ultrasonic", ultrasonic)
 
 #Default message callback. Please use custom callbacks.
 def on_message(client, userdata, msg):
     print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
+
+#Custom callbacks need to be structured with three args like on_message()
+def ultrasonic(client, userdata, message):
+    #the third argument is 'message' here unlike 'msg' in on_message 
+    print("ultrasonic: " + message.topic + " " + "\"" + 
+        str(message.payload, "utf-8") + "\"")
+    print("ultrasonic: message.payload is of type " + 
+          str(type(message.payload)))
 
 if __name__ == '__main__':
     #this section is covered in publisher_and_subscriber_example.py
@@ -23,7 +35,6 @@ if __name__ == '__main__':
     client.loop_start()
 
     while True:
-        print("delete this line")
         time.sleep(1)
             
 
