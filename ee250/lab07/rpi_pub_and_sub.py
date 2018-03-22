808c9744d47b33e32db7b83450770c4491be0985
ee250 <shmcdono@usc.edu>
2018-03-22 16:08:08 -0700

    button fix

--- ee250/lab07/rpi_pub_and_sub.py
+++ ee250/lab07/rpi_pub_and_sub.py
@@ -87,12 +87,12 @@ if __name__ == '__main__':
         client.publish("anrg-pi9/ultrasonic", data)
 
         ############### BUTTON ###############
-        # try:
-        #     button_status = digitalRead(button)	#Read the Button status
-        #     if button_status:
-        #         client.publish("anrg-pi9/button", "Button Pressed!")									
-        # except (IOError,TypeError) as e:
-        #     client.publish("anrg-pi9/button", "Button ERROR")                                    
+        try:
+            button_status = digitalRead(button_port)	#Read the Button status
+            if button_status:
+                client.publish("anrg-pi9/button", "Button Pressed!")									
+        except (IOError,TypeError) as e:
+            client.publish("anrg-pi9/button", "Button ERROR")                                    
         # button_status = digitalRead(button) #Read the Button status
         # if button_status:
         #     client.publish("anrg-pi9/button", "Button Pressed!")
